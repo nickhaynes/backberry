d6978ba2aaa80a98c9f005c53bd557d92ca6a0e4
Nick Haynes <j.n.haynes2008@gmail.com>
2018-05-20 21:28:04 -0500

    Even more testing

--- scripts/phoneBox.js
+++ scripts/phoneBox.js
@@ -5,7 +5,7 @@ const Curve8330 = {
     family:'Curve',
     model:'8330',
     series:'8000',
-    color:'color LCD',
+    color:'Color LCD',
     screenSize:'320 x 240 pixels',
     bluetooth:'no',
     carrier:'T-Mobile',
@@ -17,43 +17,63 @@ const Curve8330 = {
     price:29.08
 };
 
-let phones = [Curve8330];
-
-function createPhoneBox(phone) {
-    let phoneHTML = 
-        `<div class="phoneBox"> +
-            <h3> + ${phone.title} + </h3> +
-            <img src="images/ + ${phone.picture} + .png" alt=" + ${phone.title} + "> +
-            <ul class="details"> +
-                <li><span class="deets">Series: </span> + ${phone.series} + </li> +
-                <li><span class="deets">Screen: </span> + ${phone.screenSize} + ${phone.color}</li> +
-                <li><span class="deets">Carrier: </span> + ${phone.carrier} + </li> +
-            </ul> +
-            <p class="price"> + ${phone.price} + </p> +
-        </div>`;
-    
-    return phoneHTML;
-        
+const testModel = {
+    uniqueID:'y8476',
+    title:'tester',
+    picture:'tester',
+    family:'tester',
+    model:'tester',
+    series:'8000',
+    color:'tester LCD',
+    screenSize:'320 x 240 testers',
+    bluetooth:'no',
+    carrier:'T-Tester',
+    touchscreen:'true',
+    padOrBall:'ball',
+    extraFeatures:'SMS/MMS, Email, Push Email, IM, HTML browser, maps, MP4/WMV/H.264 player, MP3/WAV/eAAC+/WMA player, Organizaer, Voice memo/dial/commands, Predictive text input, removable battery',
+    network:'CDMA, Dual-band 800/1900 MHz CDMA2000 1X EV-DO networks',
+    shortDescription:'The Blackberry Curve brand continued the re-orientation of Blackberry to the consumer, following the Pearl and 8800 series.  The Curve 8330 was the first Curve launched for CDMA networks.',
+    price:29.08
 };
 
-function renderPhones(phones) {
-    let results = '';
-    for (phone of phones) {
-        results=createPhoneBox(phone);
-        return results;
-    };
-};
+let phones = [Curve8330, testModel];
+
+
 
 $(document).ready(function() {
 
     console.log("ready!");
+    console.log(phones.length);
 
-    createPhoneBox(phones);
+    function createPhoneBox(phone) {
+        let phoneHTML = 
+            `<div class="phoneBox">
+                <h3>${phone.title}</h3>
+                <img src="images/${phone.picture}.png" alt="${phone.title}">
+                <ul class="details">
+                    <li><span class="deets">Series: </span>${phone.series}</li>
+                    <li><span class="deets">Screen: </span>${phone.color}, ${phone.screenSize}</li>
+                    <li><span class="deets">Carrier: </span>${phone.carrier}</li>
+                </ul>
+                <p class="price">${phone.price}</p>
+            </div>`;
+        return phoneHTML;
+        console.log("I'm the renderer!");            
+    };
+    
+    function renderPhones(phones) {
+        let results = '';
+        for (let phone of phones) {
+            results=createPhoneBox(phone);
+            return results;
+        };
+        console.log("So can I!")
+    };
 
-    renderPhones(phones);
+    $( "div.placeholder" ).replaceWith(renderPhones(phones)) 
+        return $(this);
 
-    $("div.placeholder").replaceWith(renderPhones(phones));
-
+    });
+    
     console.log("OK!");
-
-});
+;
