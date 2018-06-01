3cd0c0b89e0aad35ba975cf8dfde24896e97301d
Nick Haynes <j.n.haynes2008@gmail.com>
2018-05-31 21:41:35 -0500

    Render of product pages working, updated finally

--- scripts/productBox.js
+++ scripts/productBox.js
@@ -3,29 +3,36 @@ $(document).ready(function() {
     let pIndex = mainProduct.dataset.type;
     let x;
     let p;
-    let phone;
     
     function assignPhone(p) {
-        if (p="0") {
+        if (p=="Curve8330") {
             x=0;
-        } else if (p="1") {
+        } else if (p=="Quark6230") {
             x=1;
+        } else if (p=="BoldTouch9900") {
+            x=2;
+        } else if (p=="Pearl8120") {
+            x=3;
+        } else if (p=="Curve8310") {
+            x=4;
+        } else if (p=="Bold9000") {
+            x=5;
+        } else if (p=="BlackBerry7230") {
+            x=6;
         }
-        let phone = x;
-        return phone;
+        return x;
     };
     
     assignPhone(pIndex);
 
-    console.log(pIndex);
     console.log(x);
-    console.log(mainProduct);
-    console.log(phone);
+    console.log(phoneObj[x]);
+    console.log(phoneObj[x].uniqueID);
 
 
     productCode = `
-        <h2>BlackBerry ${p.title}</h2>
-        <img src="../images/${p.picture}.png">
+        <h2>BlackBerry ${phoneObj[x].title}</h2>
+        <img src="../images/${phoneObj[x].picture}.png">
     `;
     mainProduct.innerHTML = productCode;
 });
